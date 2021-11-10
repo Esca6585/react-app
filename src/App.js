1cd4e80999679554919bd9130fe66e4df46c81e9
m.esen <m.esen@merdana.loc>
2021-11-10 09:18:00 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import Expenses from './components/Expenses';
+import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     { id: 'e1', title: 'Toyota', amount: 294.67, date: new Date(2021, 2, 28)},
     { id: 'e2', title: 'Lexus', amount: 989.31, date: new Date(2020, 5, 15)},
     { id: 'e3', title: 'Audi', amount: 345.23, date: new Date(2021, 8, 9)},
     { id: 'e4', title: 'BMW', amount: 122.35, date: new Date(2021, 11, 20)},
   ];
+
+  // return React.createElement(
+  //   'div',
+  //   {}, 
+  //   React.createElement('h2', {}, 'Let\'s get started!' ),
+  //   React.createElement(Expenses, {items: expenses})
+  // );
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
